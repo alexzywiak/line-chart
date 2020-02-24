d455b9ce5c1fc9c11ffb49fb2e214c068c48034e
Alex Zywiak <alex.zywiak@aspenmesh.io>
2020-02-24 12:01:39 -0700

    legend click changes line color

---
+++ src/BriteCharts/BriteChart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface BriteChartProps {}
+const BriteChart = (props: BriteChartProps) => {};
+
+BriteChart.displayName = "BriteChart";
+export default BriteChart;
--- src/ChartJs/Chart.tsx
+++ src/ChartJs/Chart.tsx
@@ -1,14 +1,54 @@
 import React, { useRef, useEffect } from "react";
 import moment from "moment";
-import Chart from "chart.js";
+import Chart, { ChartDataSets } from "chart.js";
 
 const randomScalingFactor = (max: number = 100, min: number = 1) =>
   Math.random() * (max - min) + min;
 const labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const configs: ChartDataSets[] = [
+  {
+    label: "My First dataset",
+    borderColor: "red",
+    backgroundColor: "red",
+    fill: false,
+    data: labels.map((n, idx) => ({
+      x: moment().subtract(idx * 5 * 60 * 1000),
+      y: randomScalingFactor()
+    })),
+    yAxisID: "y0"
+  },
+  {
+    label: "My Second dataset",
+    borderColor: "blue",
+    backgroundColor: "blue",
+    fill: false,
+    data: labels.map((n, idx) => ({
+      x: moment().subtract(idx * 5 * 60 * 1000),
+      y: randomScalingFactor(50, 1)
+    })),
+    yAxisID: "y1"
+  },
+  {
+    label: "My Third dataset",
+    borderColor: "green",
+    backgroundColor: "green",
+    fill: false,
+    pointStyle: "triangle",
+    pointRadius: 20,
+    pointRotation: 180,
+    showLine: false,
+    data: [2, 3, 5, 8].map((n, idx) => ({
+      x: moment().subtract(n * 5 * 60 * 1000),
+      y: 0
+    }))
+  }
+];
+
 export default () => {
   const chartRef = useRef<HTMLCanvasElement>(null);
   const chart = useRef<Chart | null>(null);
+  const highlightedDataIndex = useRef(0);
 
   useEffect(() => {
     if (chartRef.current && chartRef.current.getContext("2d")) {
@@ -17,66 +57,44 @@ export default () => {
         chart.current = new Chart(context, {
           type: "line",
           data: {
-            datasets: [
-              {
-                label: "My First dataset",
-                borderColor: "red",
-                backgroundColor: "red",
-                fill: false,
-                data: labels.map((n, idx) => ({
-                  x: moment().subtract(idx * 5 * 60 * 1000),
-                  y: randomScalingFactor()
-                })),
-                yAxisID: "y0"
-              },
-              {
-                label: "My Second dataset",
-                borderColor: "blue",
-                backgroundColor: "blue",
-                fill: false,
-                data: labels.map((n, idx) => ({
-                  x: moment().subtract(idx * 5 * 60 * 1000),
-                  y: randomScalingFactor(50, 1)
-                })),
-                yAxisID: "y1"
-              },
-              {
-                label: "My Third dataset",
-                borderColor: "green",
-                backgroundColor: "green",
-                fill: false,
-                pointStyle: "triangle",
-                pointRadius: 20,
-                showLine: false,
-                data: [2, 3, 5, 8].map((n, idx) => ({
-                  x: moment().subtract(n * 5 * 60 * 1000),
-                  y: 0
-                }))
+            datasets: configs.map((config, idx) => {
+              console.log(config);
+              if (highlightedDataIndex.current === idx) {
+                return config;
               }
-            ]
+              return {
+                ...config,
+                borderColor: "grey",
+                backgroundColor: "grey"
+              };
+            })
           },
           options: {
             hover: {
-              mode: "point",
-              onHover: function(event, elements) {
-                if (!elements.length) {
-                  return;
-                }
-                const yAxes =
-                  (this.options.scales && this.options.scales.yAxes) || [];
-                const activeAxis = yAxes.find(axis => Boolean(axis.display));
-                const targetId = (elements[0] as any)._yScale.id;
-                console.log(activeAxis!.id, targetId);
-                if (!activeAxis || activeAxis.id !== targetId) {
-                  this.options.scales!.yAxes = yAxes.map(axis => {
-                    const display = axis.id === targetId ? true : false;
-                    return { ...axis, display };
-                  });
-                  this.update({ duration: 500, easing: "linear" });
-                }
+              mode: "point"
+            },
+            legend: {
+              onClick: function(e, item) {
+                const t = this;
+                (this as any).chart.data.datasets = (this as any).chart.data.datasets.map(
+                  (dataSet: ChartDataSets, idx: number) => {
+                    if (idx === item.datasetIndex) {
+                      return {
+                        ...dataSet,
+                        backgroundColor: "orange",
+                        borderColor: "orange"
+                      };
+                    }
+                    return {
+                      ...dataSet,
+                      backgroundColor: "grey",
+                      borderColor: "grey"
+                    };
+                  }
+                );
+                (this as any).chart.update();
               }
             },
-
             scales: {
               xAxes: [
                 {
@@ -93,8 +111,8 @@ export default () => {
                   position: "left"
                 },
                 {
-                  id: "y1",
-                  display: false
+                  id: "y1"
+                  // display: false
                 }
               ]
             }
